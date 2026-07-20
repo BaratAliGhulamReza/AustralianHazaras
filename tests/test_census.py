b69da6966b22f4fbf3e324f05f92f6eddfad4ed6
Barat <baratalighulamreza@gmail.com>
2026-07-20 23:08:51 +1000

    Signed-off-by: Barat <baratalighulamreza@gmail.com>

--- tests/test_census.py
+++
@@ -1,9 +0,0 @@
-import json
-from pathlib import Path
-ROOT=Path(__file__).resolve().parents[1]
-def test_census_values():
-    data=json.loads((ROOT/'src/content/census.json').read_text(encoding='utf-8'))
-    for year in ('2021','2026'):
-        for key in ('hazaraAncestry','hazaragiLanguage'):
-            value=data['national'][year][key]
-            assert value is None or isinstance(value,int) and value>=0
